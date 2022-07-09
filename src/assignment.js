61776e45573eb7164304564f148965335796e552
Johnpaul Ugwulo <seanlite94@gmail.com>
2022-07-09 19:19:46 +0000

    challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        sum += arrayOfNumbers[i];
+      }
+    // console.log(sum)
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let counter = 0;
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] % 2 == 0){
+            counter ++;
+        } 
+    }
+    // console.log(counter)
+    return counter;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
